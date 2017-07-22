77c5abbbffaedf3a323faa563dd1d67e0ed453b8
Daved Artemik <dartemik@brightfind.com>
2017-07-22 00:00:55 -0500

    Add project files.

---
+++ FieldExtensions.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using SC = Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Data.Fields;
+
+using FH = Specialized.Content.FieldHelpers;
+using System.Collections;
+
+namespace Specialized.Content {
+
+    /// <summary>
+    /// Extension methods for Sitecore
+    /// </summary>
+    public static partial class FieldExtensions
+    {
+
+        /// <summary>
+        /// Set value of Field according to Field type
+        /// </summary>
+        /// <param name="field">Sitecore Field</param>
+        /// <param name="value">Value of Field</param>
+        public static void SetValueByType(this Field field, object value)
+        {
+            switch (field.Type)
+            {
+                case "Checkbox":
+                    FH.SetCheckboxValue(field, value);
+                    break;
+                case "Date":
+                case "Datetime":
+                    FH.SetDateTimeValue(field, value);
+                    break;
+                case "File":
+                    FH.SetFileValue(field, value);
+                    break;
+                case "General Link":
+                case "General Link with Search":
+                    FH.SetLinkValue(field, value);
+                    break;
+                case "Internal Link":
+                    FH.SetInternalLinkvalue(field, value);
+                    break;
+                case "Image":
+                    FH.SetImageValue(field, value);
+                    break;
+                case "Droplink":
+                case "Droplist":
+                case "Droptree":
+                case "Grouped Droplink":
+                case "Grouped Droplist":
+                    FH.SetReferenceValue(field, value);
+                    break;
+                case "Checklist":
+                case "Multilist":
+                case "Multilist with Search":
+                case "Treelist":
+                case "TreelistEx":
+                case "Version Link":
+                    FH.SetListValue(field, value);
+                    break;
+                // default handles all fields that just use Value = string;
+                default:
+                    field.Value = value.ToString();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Get Sitecore Item by value
+        /// </summary>
+        /// <param name="db">Sitecore Database</param>
+        /// <param name="value">Value as ID, GUID, Path, or Item</param>
+        /// <returns>Sitecore Item</returns>
+        public static Item GetItemFromValue(this SC.Data.Database db, object value)
+        {
+            if (value is Item)
+            {
+                return (Item)value;
+            }
+            else
+            {
+                return db.GetItem(value.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Get Sitecore Items by an enumerable of object values
+        /// </summary>
+        /// <param name="db">Sitecore Database</param>
+        /// <param name="values">Values as ID, GUID, Path, or Item</param>
+        /// <param name="ignoreNull">Disable to throw exception on null Item</param>
+        /// <returns>List of Sitecore Items</returns>
+        public static List<Item> GetItems(this SC.Data.Database db, IEnumerable values, bool ignoreNull = true)
+        {
+            List<Item> items = new List<Item>();
+            foreach (object value in values)
+            {
+                Item item = db.GetItemFromValue(value);
+                if (item == null && !ignoreNull)
+                {
+                    throw new NullReferenceException(string.Format("No item found matching {0}.", value.ToString()));
+                }
+                if (item != null)
+                {
+                    items.Add(item);
+                }
+            }
+            return items;
+        }
+
+        /// <summary>
+        /// Get Sitecore Items by a delimited string of values
+        /// </summary>
+        /// <param name="db">Sitecore Database</param>
+        /// <param name="values">Value as ID, GUID, or Path</param>
+        /// <param name="delimiter">Character delimiter to split values</param>
+        /// <param name="ignoreNull">Disable to throw exception on null Item</param>
+        /// <returns>List of Sitecore Items</returns>
+        public static List<Item> GetItems(this SC.Data.Database db, string values, char delimiter = ',', bool ignoreNull = true)
+        {
+            var valueArray = values.Split(delimiter);
+            return db.GetItems(valueArray, ignoreNull);
+        }
+
+        /// <summary>
+        /// Get children Items from Sitecore Item optionally filtered by a template
+        /// </summary>
+        /// <param name="item">Sitecore Item</param>
+        /// <param name="templateID">TemplateID to filter by</param>
+        /// <param name="recursive">Whether or not to return all children or only immediate children</param>
+        /// <returns></returns>
+        public static List<Item> GetChildren(this Item item, ID templateID = null, bool recursive = false)
+        {
+            if (recursive)
+            {
+                return item.Axes.SelectItems(string.Format("descendant::*{0}", (!templateID.Equals(null) ? string.Format("[@@templateid='{0}']", templateID.ToString()) : ""))).ToList();
+            }
+            else
+            {
+                return item.Children.ToList();
+            }
+        }
+
+        /// <summary>
+        /// Get Sitecore Fields that are not System Fields for Item 
+        /// </summary>
+        /// <param name="item">Sitecore Item</param>
+        /// <returns>Editable fields</returns>
+        public static IEnumerable<Field> GetEditableFields(this Item item)
+        {
+            item.Fields.ReadAll();
+            return item.Fields.Where(f => !f.Name.StartsWith("__"));
+        }
+
+        /// <summary>
+        /// Render field from current item
+        /// </summary>
+        /// <param name="item">Sitecore Item</param>
+        /// <param name="fieldName">Name of field to render</param>
+        /// <param name="parameters">Optional parameters for rendering</param>
+        /// <returns>Rendering string</returns>
+        public static string RenderField(this Item item, string fieldName, string parameters = "")
+        {
+            if (item.Fields[fieldName] != null)
+            {
+                return SC.Web.UI.WebControls.FieldRenderer.Render(item, fieldName, parameters);
+            }
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Render current Field with value from Item the Field is from
+        /// </summary>
+        /// <param name="field">Sitecore Field</param>
+        /// <param name="parameters">Optional parameters for rendering</param>
+        /// <returns>Rendering string</returns>
+        public static string RenderField(this Field field, string parameters = "")
+        {
+            if (field.Item != null)
+            {
+                return SC.Web.UI.WebControls.FieldRenderer.Render(field.Item, field.Name, parameters);
+            }
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Get Sitecore Fields that are not System Fields for TemplateItem
+        /// </summary>
+        /// <param name="item">Sitecore TemplateItem</param>
+        /// <returns>Editable fields</returns>
+        public static IEnumerable<TemplateFieldItem> GetEditableFields(this TemplateItem item)
+        {
+            return item.Fields.Where(f => !f.Name.StartsWith("__"));
+        }
+    }
+}
---
+++ FieldHelpers.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using SC = Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Data.Fields;
+
+namespace Specialized.Content{
+
+	/// <summary>
+	/// Helper methods specific to fields
+	/// </summary>
+	public static class FieldHelpers {
+
+		/// <summary>
+		/// Set URL for General Link field with default link type set as external
+		/// </summary>
+		/// <param name="field">Sitecore Field</param>
+		/// <param name="value">URL, Item Path, or Item ID</param>
+		/// <param name="linkType">Type of link as string</param>
+		public static void SetLinkValue(Field field, object value, string linkType = "external") {
+			LinkField thisField = (LinkField) field;
+			if (linkType == "media" || linkType == "internal") {
+				Item item = field.Database.GetItemFromValue(value);
+				if (item != null) {
+					if (linkType == "media") {
+						thisField.Url = item.Paths.MediaPath;
+					}
+					else {
+						thisField.Url = SC.Links.LinkManager.GetItemUrl(item, SC.Links.LinkManager.GetDefaultUrlOptions());
+					}
+					thisField.TargetID = item.ID;
+				}
+				else { thisField.Url = value.ToString(); }
+			}
+			else {
+				thisField.Url = value.ToString();
+			}
+			thisField.LinkType = linkType;
+		}
+
+		/// <summary>
+		/// Set URL for Internal Link field
+		/// </summary>
+		/// <param name="field">Sitecore Field</param>
+		/// <param name="value">Path or ID of existing Sitecore Item</param>
+		public static void SetInternalLinkvalue(Field field, object value) {
+			InternalLinkField thisField = (InternalLinkField) field;
+			Item item = field.Database.GetItemFromValue(value);
+			if (item != null) {
+				thisField.Value = item.Paths.Path;
+			}
+		}
+
+		/// <summary>
+		/// Set value for Date and DateTime field
+		/// </summary>
+		/// <param name="field">Sitecore Field</param>
+		/// <param name="value">Date string or DateTime object</param>
+		public static void SetDateTimeValue(Field field, object value) {
+			DateField thisField = (DateField) field;
+			DateTime dateVal = new DateTime();
+			if (DateTime.TryParse(value.ToString(), out dateVal)) {
+				thisField.Value = SC.DateUtil.ToIsoDate(dateVal);
+			}
+		}
+
+		/// <summary>
+		/// Set checked state on Checkbox field
+		/// </summary>
+		/// <param name="field">Sitecore Field</param>
+		/// <param name="value">Boolean, True/False string, or Integer</param>
+		public static void SetCheckboxValue(Field field, object value) {
+			CheckboxField thisField = (CheckboxField) field;
+			try {
+				int boolInt = 0;
+				bool boolVal;
+				if (value.ToString().Equals("yes", StringComparison.OrdinalIgnoreCase) || value.ToString().Equals("no", StringComparison.OrdinalIgnoreCase)) {
+					boolVal = (value.ToString().Equals("yes", StringComparison.OrdinalIgnoreCase) ? true : false);
+				}
+				else if (int.TryParse(value.ToString(), out boolInt)) {
+					boolVal = Convert.ToBoolean(boolInt);
+				}
+				else { boolVal = Convert.ToBoolean(value); }
+				thisField.Checked = boolVal;
+			}
+			catch (Exception ex) { /* do nothing for now */ }
+		}
+
+		/// <summary>
+		/// Set value for File field
+		/// </summary>
+		/// <param name="field">Sitecore Field</param>
+		/// <param name="value">Path or ID of existing Sitecore Item</param>
+		public static void SetFileValue(Field field, object value) {
+			FileField thisField = (FileField) field;
+			Item item = field.Database.GetItemFromValue(value);
+
+			if (item != null) {
+				thisField.MediaID = item.ID;
+				thisField.Src = SC.Resources.Media.MediaManager.GetMediaUrl(item);
+			}
+		}
+
+		/// <summary>
+		/// Set value for Image field
+		/// </summary>
+		/// <param name="field">Sitecore Field</param>
+		/// <param name="value">Path or ID of existing Sitecore Item</param>
+		public static void SetImageValue(Field field, object value) {
+			ImageField thisField = (ImageField) field;
+			Item item = field.Database.GetItemFromValue(value);
+
+			if (item != null) {
+				MediaItem mediaItem = new MediaItem(item);
+				thisField.MediaID = mediaItem.ID;
+				if (!string.IsNullOrWhiteSpace(mediaItem.Alt)) {
+					thisField.Alt = mediaItem.Alt;
+				}
+				else {
+					thisField.Alt = mediaItem.DisplayName;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Set value for Droplist, Droplink, and Droptree field types
+		/// </summary>
+		/// <param name="field">Sitecore Field</param>
+		/// <param name="value">Path or ID of existing Sitecore Item</param>
+		public static void SetReferenceValue(Field field, object value) {
+			ReferenceField thisField = (ReferenceField) field;
+			Item item = field.Database.GetItemFromValue(value);
+
+			if (item != null) {
+				if (field.Type == "Droplist") {
+					thisField.Value = item.Name;
+				}
+				else {
+					thisField.Value = item.ID.ToString();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Set value for Checklist, Multilist, Treelist, and Version Link field types
+		/// </summary>
+		/// <param name="field">Sitecore Field</param>
+		/// <param name="value">Sitecore Item, IEnumerable of Item, IEnumerable of Path or IDs as string, ID as GUID or Sitecore.Data.ID, Path or ID as string</param>
+		public static void SetListValue(Field field, object value) {
+			MultilistField thisField = (MultilistField) field;
+
+			if (value is Item) {
+				thisField.Value = ((Item) value).ID.ToString();
+			}
+			else if (value is IEnumerable<Item>) {
+				thisField.Value = string.Join("|", ((IEnumerable<Item>) value).Select(i => i.ID.ToString()));
+			}
+			else if (value is IEnumerable<string>) {	// assume string is path or guid
+				thisField.Value = string.Empty;	// clear value for add
+				foreach (var val in (IEnumerable<string>) value) {
+					Item item = field.Database.GetItemFromValue(val);
+					if (item != null) {
+						thisField.Add(item.ID.ToString());
+					}
+				}
+				thisField.Value = string.Join("|", (IEnumerable<string>) value);
+			}
+			else if (value is Guid || value is SC.Data.ID) {
+				thisField.Value = value.ToString();
+			}
+			else if (value is string) {
+				string val = value.ToString().ToUpper();	// fixes "Item not in selection" mismatch due to lowercase
+				if (val.Contains(',')) {
+					thisField.Value = val.Replace(',', '|');
+				}
+				else if (val.Contains('|')) {
+					thisField.Value = val;
+				}
+				else {
+					Item item = field.Database.GetItemFromValue(value);
+					if (item != null) {
+						thisField.Value = item.ID.ToString();
+					}
+				}
+			}
+		}
+	}
+}
